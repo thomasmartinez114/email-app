70c6b20bb539016d6d8e029e6b3b1eba03388f5c
Thomas Martinez <tmartinez114@gmail.com>
2020-11-17 23:29:13 -0500

    Set department of new email user

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -11,9 +13,30 @@ public class Email {
         this.firstName = firstName; // this.firstName refers to Class Level above, firstName local
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // Call a method asking for department - return department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
     }
 
     // Ask for department
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development" +
+                "\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if (depChoice == 1) {
+            return "Sales";
+        } else if(depChoice == 2) {
+            return "Development";
+        } else if (depChoice == 3) {
+            return "Accounting";
+        } else {
+            return "";
+        }
+    }
 
     // Generate random password
 
