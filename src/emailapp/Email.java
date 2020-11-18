12161257b9c6109f1c5dd1733b03cb0148e2b0d6
Thomas Martinez <tmartinez114@gmail.com>
2020-11-17 23:02:59 -0500

    set variables

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,4 +1,10 @@
 package emailapp;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 }
